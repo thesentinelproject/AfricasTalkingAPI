ddc9c8c5621cd5490a1c2e0f63404c32014d5726
Abhishek Bhatnagar <abhishekbh@gmail.com>
2014-06-02 19:35:45 -0700

    recurse for messages

--- app/controllers/messages.js
+++ app/controllers/messages.js
@@ -9,6 +9,9 @@ var mongoose = require('mongoose')
   , http = require('http')
   , env = process.env.NODE_ENV || 'development'
   , config = require('../../config/config')[env]
+  , querystring = require('querystring')
+  , jsesc = require('jsesc')
+  , https = require('https');
 
 /**
  * Load
@@ -39,54 +42,13 @@ exports.index = function(req, res){
   //
   //////////////////////////////////////////////////////////////////////////////////////
   // get latest data from API
-  var latest_id;
-
   Message.findOne({}, {}, { sort: {'created_at': -1}}, function(err, message) {
     if (null == message) {
-      latest_id = 0
+      fetchMessages(0);
     } else {
+      fetchMessages(message.extern_id);
       latest_id = message.extern_id
     };
-  });
-
-  var req_options = {
-    hostname: 'api.africastalking.com',
-    path: '/version1/messaging?username='+config.africastalking.username+'&lastReceivedId='+latest_id,
-    method: 'GET',
-    headers: {
-      'ApiKey': config.africastalking.apiKey
-      ,'Accept':'application/json'
-    }
-  };
-
-  var req = http.get(req_options, function(res){
-    var bodyChunks = [];
-    res.on('data', function(chunk) {
-      bodyChunks.push(chunk);
-    }).on('end', function() {
-      var body = Buffer.concat(bodyChunks);
-      var parsedBody = JSON.parse(body);
-      var new_messages = parsedBody['SMSMessageData']['Messages'];
-
-      if (new_messages != null) {
-        Object.keys(new_messages).forEach(function(key) {
-          var organic = new_messages[key];
-          // construct and save message
-          new Message({
-            extern_id: organic['id'],
-            text: organic['text'],
-            from: organic['from'],
-            to: organic['to'],
-            linkId: organic['linkId'],
-            date: organic['date']
-          }).save();
-        });
-      }
-    })   
-  })
-  .on('error',function(e){
-    console.log("Error: " + req_options.hostname + "\n" + e.message); 
-    console.log(e.stack);
   });
   //////////////////////////////////////////////////////////////////////////////////////
   // Resume normal ops
@@ -103,6 +65,45 @@ exports.index = function(req, res){
     })
   })
 }
+
+function fetchMessages(lastReceivedId_) {
+  var options = {
+    host: 'api.africastalking.com',
+    port: '443',
+    path: '/version1/messaging?username=' + config.africastalking.username + '&lastReceivedId=' + lastReceivedId_,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'apikey': config.africastalking.apiKey
+    }
+  };
+    
+  var request = https.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      var jsObject = JSON.parse(chunk);
+      var messages = jsObject.SMSMessageData.Messages;
+
+      if (messages.length > 0) {
+        for (var i = 0; i < messages.length; ++i) {
+          // construct and save message
+          new Message({
+            extern_id: messages[i].id,
+            text: jsesc(messages[i].text),
+            from: messages[i].from,
+            to: messages[i].to,
+            linkId: messages[i].linkId, 
+            date: messages[i].date
+          }).save();
+        }
+        // recurse baby recurse
+        fetchMessages(lastReceivedId_);
+      } 
+    });
+  });
+  request.end();
+};
+
 
 /**
  * Show
